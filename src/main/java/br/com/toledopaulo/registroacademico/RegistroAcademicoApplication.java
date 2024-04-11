2c51208309bcc6c9c2b67c31d58794f6668adeba
Paulo <paulo.aff1803@gmail.com>
2024-04-11 15:18:59 -0300

    feat: projeto spring inicial

---
+++ src/main/java/br/com/toledopaulo/registroacademico/RegistroAcademicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.toledopaulo.registroacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroAcademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/toledopaulo/registroacademico/RegistroAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.toledopaulo.registroacademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
